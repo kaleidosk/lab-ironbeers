afd5054ac0feefe64c97f8c13f61513054ea31fe
Jennifer Manella <jennifermanella@MacBook-Pro-de-Jennifer.local>
2023-11-30 13:31:19 +0100

    done

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -22,4 +23,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/Beers', (req,res, next)=> {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+  console.log('Beers from the database: ', beersFromApi)
+res.render ('beers.hbs', {beers:beersFromApi })
+})
+.catch(error => console.log(error));
+})
+
+app.get('/Random',(req,res,next)=>{
+  punkAPI
+  .getRandom()
+  .then((responseFromApi) => {
+    res.render('random-beer.hbs', {beers:responseFromApi})
+  })
+  .catch(error => console.log(error));
+})
+
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'))
+
